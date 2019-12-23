91787490c5c5fa90219b6ceed8a3873c987cd819
Iharob Al Asimi <iharob@gmail.com>
2019-12-23 08:37:17 -0400

    Removed useless log

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -44,7 +44,6 @@ export const Table: (props: Props) => (React.ReactElement | null) = (props: Prop
   useEffect(() => {
     if (!ref || !rows)
       return;
-    console.log('rows changed');
     setHeight(ref.offsetHeight);
     const observer: ResizeObserver = new ResizeObserver(() => {
       setHeight(ref.offsetHeight);
