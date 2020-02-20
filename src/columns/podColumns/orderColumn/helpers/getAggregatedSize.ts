936407fa51aefde1a7335ed5125c598ebbd3af94
Iharob Al Asimi <iharob@gmail.com>
2020-02-20 16:44:07 -0400

    Change id of cancelled orders to remove them from the cache

--- src/columns/podColumns/orderColumn/helpers/getAggregatedSize.ts
+++ src/columns/podColumns/orderColumn/helpers/getAggregatedSize.ts
@@ -5,6 +5,10 @@ import {priceFormatter} from 'utils/priceFormatter';
 
 export const getAggregatedSize = (aggregatedSize: AggregatedSize | undefined, order: Order): number | null => {
   const orders: Order[] = SignalRManager.getDepthOfTheBook(order.symbol, order.strategy, order.tenor, order.type);
+  if (orders.length === 0)
+    return null;
+  else if (orders.length === 1)
+    return orders[0].size;
   return orders
     .filter((other: Order) => priceFormatter(other.price) === priceFormatter(order.price))
     .reduce((size: number, order: Order) => size + ((order.size === null) ? 0 : order.size), 0);
