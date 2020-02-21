82b76ec3526270b8e2323b7aeb034f31c734f817
Iharob Al Asimi <iharob@gmail.com>
2020-02-21 10:45:08 -0400

    Fix random out of workspace resizes

--- src/components/PodTile/Row/helpers/replaceOrder.ts
+++ src/components/PodTile/Row/helpers/replaceOrder.ts
@@ -2,12 +2,16 @@ import {Order} from 'interfaces/order';
 import {OrderTypes} from 'interfaces/mdEntry';
 import {State} from 'components/PodTile/Row/reducer';
 
+/**
+ * Replacing an order shouldn't overwrite the dark price ever
+ */
 export const replaceOrder = (state: State, order: Order): State => {
   const {internalRow} = state;
+  const {darkPrice} = internalRow;
   if (order.type === OrderTypes.Bid) {
-    return {...state, internalRow: {...internalRow, bid: order}};
+    return {...state, internalRow: {...internalRow, bid: order, darkPrice}};
   } else if (order.type === OrderTypes.Ofr) {
-    return {...state, internalRow: {...internalRow, ofr: order}};
+    return {...state, internalRow: {...internalRow, ofr: order, darkPrice}};
   } else {
     return state;
   }
--- src/components/WindowManager/window.tsx
+++ src/components/WindowManager/window.tsx
@@ -32,10 +32,12 @@ const toStyle = (geometry: ClientRect | undefined): CSSProperties | undefined =>
 };
 
 const resize = (x: number, y: number, width: number, height: number, r: ClientRect, minWidth: number): DOMRect => {
-  if (width < minWidth)
-    width = minWidth;
   const left: number = Math.min(Math.max(x, r.left), r.right - Math.min(width, r.width));
   const top: number = Math.min(Math.max(y, r.top), r.bottom - Math.min(height, r.height));
+  if (width - left > window.innerWidth) {
+    console.log(left, top, width, height);
+    return new DOMRect(0, 0, 100, 100);
+  }
   // Create the new rectangle confined to the r rectangle
   return new DOMRect(left, top, width, height);
 };
