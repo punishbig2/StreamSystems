074af67ca80648c4d40b1c661cba67a834229972
Iharob Al Asimi <iharob@gmail.com>
2020-04-13 13:31:54 -0400

    Tab order fixed in POD

--- src/columns/podColumns/OrderColumn/helpers/moveToNextPrice.ts
+++ src/columns/podColumns/OrderColumn/helpers/moveToNextPrice.ts
@@ -5,7 +5,7 @@ export const moveToNextPrice = (input: HTMLInputElement, tabDirection: TabDirect
   // Move to the next price
   const parent: HTMLElement | null = getNthParentOf(input, 7);
   if (parent !== null) {
-    const inputs: HTMLInputElement[] = Array.from(parent.querySelectorAll('.price-layout input'));
+    const inputs: HTMLInputElement[] = Array.from(parent.querySelectorAll(':not(.dark-pool-base).price-layout input'));
     const index: number = inputs.indexOf(input);
     if (index === -1)
       throw new Error('self has to have a non -1 index, this is crazy');
