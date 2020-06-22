3a239769812597906a1b9562d44adb89c95341b3
Iharob Al Asimi <iharob@gmail.com>
2020-06-22 15:26:09 -0400

    Fix the issue with blank screen when a new deal comes in

--- src/mobx/stores/dealsStore.ts
+++ src/mobx/stores/dealsStore.ts
@@ -1,18 +1,24 @@
 import { Deal } from "components/MiddleOffice/DealBlotter/deal";
 import { observable, action } from "mobx";
 import { API } from "API";
+import { parseTime } from "timeUtils";
 
 export class DealsStore {
   @observable.ref deals: Deal[] = [];
 
   constructor() {
-    API.getDeals().then((deals: any) => {
-      this.deals = deals.sort((d1: Deal, d2: Deal) => Number(d2.transactionTime) - Number(d1.transactionTime));
+    API.getDeals().then((deals: Deal[]) => {
+      this.deals = deals.sort(
+        (d1: Deal, d2: Deal) =>
+          parseTime(d2.transactionTime, "Z").getTime() -
+          parseTime(d1.transactionTime, "Z").getTime()
+      );
     });
   }
 
   @action.bound
   public addDeal(deal: Deal) {
+    console.log(deal);
     this.deals = [deal, ...this.deals];
   }
 }
