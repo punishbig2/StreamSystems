a6971383c2814cd33b3b7c11189f4325aa5da6e1
Iharob Al Asimi <iharob@gmail.com>
2020-09-09 11:11:22 -0400

    Fix units issue

--- src/API/index.ts
+++ src/API/index.ts
@@ -865,7 +865,7 @@ export class API {
       strategy: data.strategy,
       symbol: data.symbol,
       spread: data.spread,
-      vol: data.vol,
+      vol: data.vol !== null ? data.vol / 100 : null,
       lastqty: data.size,
       notional1: data.notional1,
       lvsqty: "0",
