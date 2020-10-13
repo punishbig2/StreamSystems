58e62fbdcffd43011c32c9b94819097148b14bbd
Iharob Al Asimi <iharob@gmail.com>
2020-10-13 15:30:14 -0400

    Redirect to login on failure to load

--- src/components/MiddleOffice/hooks/useMoInitializer.ts
+++ src/components/MiddleOffice/hooks/useMoInitializer.ts
@@ -1,8 +1,15 @@
 import moStore from "mobx/stores/moStore";
 import { useEffect } from "react";
+import config from "config";
 
 export const useMoInitializer = () => {
   useEffect(() => {
-    moStore.loadReferenceData().then(() => {});
+    moStore
+      .loadReferenceData()
+      .then(() => {})
+      .catch((error: any): void => {
+        const { location } = window;
+        location.href = config.SignOutUrl;
+      });
   }, []);
 };
