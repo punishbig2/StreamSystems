c707e81737031d2d2172eaab61625b67fed58fff
Iharob Al Asimi <iharob@gmail.com>
2020-10-15 09:16:55 -0400

    Add idle timeout

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,31 @@ import { Workarea } from "components/Workarea";
 import { observer } from "mobx-react";
 import { themeStore } from "mobx/stores/themeStore";
 import React, { useEffect } from "react";
-import { createTheme } from "theme";
+import config from "./config";
+import { createTheme } from "./theme";
 
 const FXOptionsUI: React.FC = observer(
   (): React.ReactElement => {
     const { theme } = themeStore;
+    useEffect(() => {
+      const timer = setTimeout((): void => {
+        const { location } = window;
+        location.href = config.SignOutUrl;
+      }, config.IdleTimeout);
+      const events: string[] = ["click", "mousemove", "keyup", "keydown"];
+      const reset = (): void => {
+        events.forEach((event: string): void => {
+          document.removeEventListener(event, reset);
+        });
+        clearTimeout(timer);
+      };
+      events.forEach((event: string): void => {
+        document.addEventListener(event, reset);
+      });
+      return (): void => {
+        clearTimeout(timer);
+      };
+    });
     useEffect(() => {
       const { body } = document;
       body.setAttribute("class", theme + "-theme");
