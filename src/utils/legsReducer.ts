9f8c3e77d47626730445c7d744984652e230f68e
Iharob Al Asimi <iharob@gmail.com>
2021-07-26 19:19:45 -0400

    Fixes legs issues

--- src/utils/legsReducer.ts
+++ src/utils/legsReducer.ts
@@ -1,62 +1,88 @@
+import { isTenor } from "components/FormField/helpers";
 import { Leg } from "components/MiddleOffice/types/leg";
 import { DealEntry } from "types/dealEntry";
-import { InvalidTenor, Tenor } from "types/tenor";
+import { tryToNumber } from "utils/commonUtils";
 import { isNumeric } from "utils/isNumeric";
 
-export const legsReducer =
-  (
-    index: number,
-    partial: Partial<DealEntry>,
-    entry: DealEntry
-  ): ((leg: Leg, field: string) => Leg) =>
-  (leg: Leg, field: string): Leg => {
-    const key: keyof DealEntry = field as keyof DealEntry;
-    switch (key) {
-      case "tenor1":
-        const tenor: Tenor | InvalidTenor | undefined = partial[key];
-        if (tenor !== undefined) {
-          return {
-            ...leg,
-            deliveryDate: tenor.deliveryDate,
-            expiryDate: tenor.expiryDate,
-          };
-        }
-        break;
-      case "tenor2":
-        if (index === 1) {
-          const tenor: string | Tenor | undefined | null = partial[key];
-          if (typeof tenor === "string") return leg;
-          if (tenor !== undefined && tenor !== null) {
-            return {
-              ...leg,
-              deliveryDate: tenor.deliveryDate,
-              expiryDate: tenor.expiryDate,
-            };
-          }
-        }
-        break;
-      case "not1":
-        if (index === 0) {
-          return { ...leg, notional: partial[key] };
-        } else if (index === 1 && !isNumeric(entry.not2)) {
-          return { ...leg, notional: partial[key] };
-        } else {
-          return leg;
-        }
-      case "not2":
-        if (index !== 1) return leg;
+const isNotional = (value: any): value is number | undefined | null => {
+  return (
+    (typeof value === "string" && isNumeric(value)) ||
+    value === undefined ||
+    value === null
+  );
+};
+
+export const legsReducer = (
+  index: number,
+  partial: Partial<DealEntry>,
+  entry: DealEntry
+): ((leg: Leg, field: string) => Leg) => (leg: Leg, field: string): Leg => {
+  const key: keyof DealEntry = field as keyof DealEntry;
+  const value = partial[key];
+  switch (key) {
+    case "tenor1":
+      if (isTenor(value)) {
         return {
           ...leg,
-          notional: isNumeric(partial[key]) ? partial[key] : entry.not1,
+          deliveryDate: value.deliveryDate,
+          expiryDate: value.expiryDate,
         };
-      case "premstyle":
-        return { ...leg };
-      case "deltastyle":
-        return { ...leg };
-      case "dealstrike":
-        return { ...leg, strike: partial[key] };
-      default:
+      }
+      break;
+    case "tenor2":
+      if (index === 1) {
+        if (isTenor(value)) {
+          return {
+            ...leg,
+            deliveryDate: value.deliveryDate,
+            expiryDate: value.expiryDate,
+          };
+        }
+      }
+      break;
+    case "not1":
+      if (index === 0) {
+        if (isNotional(value)) {
+          return { ...leg, notional: value };
+        } else {
+          console.warn(`expecting '${value}' to be number`);
+          return leg;
+        }
+      } else if (index === 1 && isNotional(value) && !isNumeric(entry.not2)) {
+        return { ...leg, notional: value };
+      } else {
         return leg;
-    }
-    return leg;
-  };
+      }
+    case "not2":
+      if (index !== 1) return leg;
+      return {
+        ...leg,
+        notional: isNumeric(partial[key]) ? partial[key] : entry.not1,
+      };
+    case "premstyle":
+      return { ...leg };
+    case "deltastyle":
+      return { ...leg };
+    case "vol":
+      if (
+        (typeof value === "string" || typeof value === "number") &&
+        isNumeric(value)
+      ) {
+        return { ...leg, vol: value };
+      } else {
+        return leg;
+      }
+    case "dealstrike":
+      if (typeof value === "string") {
+        const numeric = tryToNumber(value);
+        return { ...leg, strike: numeric };
+      } else if (typeof partial[key] === "number") {
+        return { ...leg, strike: partial[key] };
+      } else {
+        return { ...leg, strike: partial[key] };
+      }
+    default:
+      return leg;
+  }
+  return leg;
+};
