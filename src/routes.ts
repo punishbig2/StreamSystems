8a2aabfe98323e91cffc6ae0664b5072ffeed423
Iharob Al Asimi <iharob@gmail.com>
2021-11-01 07:54:48 -0400

    Remove obsolete files

--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-export const USER_NOT_FOUND_ROUTE = "/user-not-found";
-export const ACCESS_DENIED_ROUTE = "/access-denied";
-export const GENERIC_ERROR_ROUTE = "/error";
-export const ACTIVE_WORKSPACE_ROUTE = "/workspace/:type/:index";
-export const TRADING_SCHEDULE_ERROR_ROUTE = "/inactive";
-export const LOADING_ROUTE = "/";
-export const WELCOME_ROUTE = "/welcome";
